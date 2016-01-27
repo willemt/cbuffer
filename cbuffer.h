00eed6a5c92851891d0377ef7adf6aca388fad3f
Willem Thiart <himself@willemthiart.com>
2016-01-27 13:31:10 +0200

    Improve documentation

--- cbuffer.h
+++ cbuffer.h
@@ -8,40 +8,67 @@ typedef struct
     void *data;
 } cbuf_t;
 
-/**
- * creat new circular buffer.
- * @param order to the power of two equals size*/
+/** Create new circular buffer.
+ * @param order Size of the circular buffer when taking this as a power of 2.
+ * @return pointer to new circular buffer */
 cbuf_t *cbuf_new(const unsigned int order);
 
+/** Free memory used by circular buffer
+ * @param cb The circular buffer. */
 void cbuf_free(cbuf_t* cb);
 
-/**
+/** Write data to the tail of the circular buffer.
+ * Increases the position of the tail.
+ * This copies data to the circular buffer using memcpy.
+ * After calling you should free the data if you don't need it anymore.
+ *
+ * @param cb The circular buffer.
+ * @param data Buffer to be written to the circular buffer.
+ * @param size Size of the buffer to be added to the circular buffer in bytes.
  * @return number of bytes offered */
 int cbuf_offer(cbuf_t* cb, const unsigned char *data, const int size);
 
-/**
- * @return tell us how much space we have assigned */
-int cbuf_usedspace(const cbuf_t* cb);
-
-/**
- * @return tell us how much space we can assign */
-int cbuf_unusedspace(const cbuf_t* cb);
-
-/**
- * Look at data.
- * Don't move cursor */
+/** Look at data at the circular buffer's head.
+ * Use cbuf_usedspace to determine how much data in bytes can be read.
+ * @param cb The circular buffer.
+ * @return pointer to the head of the circular buffer */
 unsigned char *cbuf_peek(const cbuf_t* cb);
 
-/**
- * Get pointer to data to read. Move the cursor on.
+/** Release data at the head from the circular buffer.
+ * Increase the position of the head.
  *
- * @return pointer to data, null if we can't poll this much data */
+ * WARNING: this is a dangerous call if:
+ *  1. You are using the returned data pointer.
+ *  2. Another thread has offerred data to the circular buffer.
+ *
+ * If you want to access the data from the returned  pointer you are better off
+ * using cbuf_peek.
+ *
+ * @param cb The circular buffer.
+ * @param size Number of bytes to release
+ * @return pointer to data; NULL if we can't poll this much data */
 unsigned char *cbuf_poll(cbuf_t *cb, const unsigned int size);
 
-void cbuf_poll_release(cbuf_t* cb, const int size);
-
+/** Size in bytes of the circular buffer.
+ * Is equal to 2 ^ order.
+ *
+ * @param cb The circular buffer.
+ * @return size of the circular buffer in bytes */
 int cbuf_size(const cbuf_t* cb);
 
+/** Tell how much data has been written in bytes to the circular buffer.
+ * @param cb The circular buffer.
+ * @return number of bytes of how data has been written */
+int cbuf_usedspace(const cbuf_t* cb);
+
+/** Tell how much data we can write in bytes to the circular buffer.
+ * @param cb The circular buffer.
+ * @return number of bytes of how much data can be written */
+int cbuf_unusedspace(const cbuf_t* cb);
+
+/** Tell if the circular buffer is empty.
+ * @param cb The circular buffer.
+ * @return 1 if empty; otherwise 0 */
 int cbuf_is_empty(const cbuf_t* cb);
 
 #endif /* CBUFFER_H */
